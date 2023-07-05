c124832b3e9e2ae37e485ada0ded433c65ec01f5
Sahil_Saxena <sahil.2024it1169@kiet.edu>
2023-07-05 15:41:37 +0530

    Create Grid Unique Paths.cpp

---
+++ Arrays Part-III/Grid Unique Paths.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n, int i = 0, int j = 0) {
+        if(i >= m || j >= n) return 0;                                   
+        if(i == m-1 && j == n-1) return 1;                               
+        return uniquePaths(m, n, i+1, j) + uniquePaths(m, n, i, j+1);     
+    }
+};

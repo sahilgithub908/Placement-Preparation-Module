c91abffe40eb403a2b828928d79ddabbcc7f7c1b
Sahil_Saxena <sahil.2024it1169@kiet.edu>
2023-07-06 20:21:44 +0530

    Update 4-Sum Problem.java

--- Arrays Part-IV/4-Sum Problem.java
+++ Arrays Part-IV/4-Sum Problem.java
@@ -2,15 +2,15 @@ class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> quadruplets = new ArrayList<>();
         int n = nums.length;
-        // Sorting the array
+        
         Arrays.sort(nums);
         for (int i = 0; i < n - 3; i++) {
-            // Skip duplicates
+        
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
             for (int j = i + 1; j < n - 2; j++) {
-                // Skip duplicates
+            
                 if (j > i + 1 && nums[j] == nums[j - 1]) {
                     continue;
                 }
@@ -24,7 +24,7 @@ class Solution {
                         right--;
                     } else {
                         quadruplets.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
-                        // Skip duplicates
+                        
                         while (left < right && nums[left] == nums[left + 1]) {
                             left++;
                         }

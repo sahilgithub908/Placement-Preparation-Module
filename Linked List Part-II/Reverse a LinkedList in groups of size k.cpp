9d79288cb49caf51fae47cdcb91fe71b35aba7fc
Sahil_Saxena <sahil.2024it1169@kiet.edu>
2023-07-08 18:52:23 +0530

    Create Reverse a LinkedList in groups of size k.cpp

---
+++ Linked List Part-II/Reverse a LinkedList in groups of size k.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+
+        if(head == NULL) return NULL;
+
+        ListNode* next = NULL;
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        int count = 0;
+
+        // edge case
+        ListNode* ptr = head;
+        for(int i = 0; i < k; i++) {
+            if(ptr == NULL) return head;
+            ptr = ptr->next;
+        }
+
+
+        while(curr != NULL && count < k){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+            count++;
+        }
+
+        if(next != NULL) head->next = reverseKGroup(next,k);
+        
+        return prev;
+    }
+};

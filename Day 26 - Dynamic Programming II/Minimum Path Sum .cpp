8ec6315a6a962542c035fb2730cad9ea83e1e4be
Sahil_Saxena <sahil.2024it1169@kiet.edu>
2023-08-02 16:52:35 +0530

    Create Minimum Path Sum .cpp

---
+++ Day 26 - Dynamic Programming II/Minimum Path Sum .cpp
@@ -0,0 +1,19 @@
+int minSumPath(vector<vector<int>> &grid) {
+    int n = grid.size(), m = grid[0].size();
+    
+    vector<vector<int>> dp(n,vector<int>(m));
+    
+    for(int i = n-1; i >= 0 ; i--){
+        for(int j = m-1; j >= 0 ; j--){
+            if(i == n-1 && j == m-1)
+                dp[i][j] = grid[i][j];
+            else if(i == n-1) 
+                dp[i][j] = grid[i][j] + dp[i][j+1];
+            else if(j == m-1)
+                dp[i][j] = grid[i][j] + dp[i+1][j];
+            else
+                dp[i][j] = grid[i][j] + min(dp[i+1][j],dp[i][j+1]);
+        }
+    }
+    return dp[0][0];
+}
